8c6be0b1be7d20cbb129b05f2257d4e65fea8aec
Hiuvek <160373690+Hiuvek@users.noreply.github.com>
2026-08-04 20:55:40 -0600

    Funcionalidad al juego

--- script.js
+++ script.js
@@ -9,11 +9,65 @@ function processInput(userInput) {
 }
 function randomChoice() {
   const randomInt = Math.floor(Math.random() * (4 - 1)) + 1;
-  return randomInt;
+  switch (randomInt) {
+    case 1:
+      return "piedra";
+    case 2:
+      return "papel";
+    case 3:
+      return "tijera";
+  }
 }
-for (let i = 0; i < 3; i++) {
-  let userInput = getUserInput();
-  let processedInput = processInput(userInput);
-  console.log("User Input:", processedInput);
-  console.log("Random Choice:", randomChoice());
+// Código principal
+function playGame() {
+  for (let i = 0; i < 3; i++) {
+    let userInput = getUserInput();
+    let processedInput = processInput(userInput);
+    console.log("Elegiste:", processedInput);
+    console.log("Pc Elige:", randomChoice());
+    contHumano = 0;
+    contPc = 0;
+    //Papel
+    if (processedInput === "papel" && randomChoice() === "tijera") {
+      console.log("Perdiste!");
+      contPc++;
+    } else if (processedInput === "papel" && randomChoice() === "piedra") {
+      console.log("Ganaste!");
+      contHumano++;
+    } else if (processedInput === "papel" && randomChoice() === "papel") {
+      console.log("Empate!");
+    }
+    //Piedra
+    if (processedInput === "piedra" && randomChoice() === "papel") {
+      console.log("Perdiste!");
+      contPc++;
+    } else if (processedInput === "piedra" && randomChoice() === "tijera") {
+      console.log("Ganaste!");
+      contHumano++;
+    } else if (processedInput === "piedra" && randomChoice() === "piedra") {
+      console.log("Empate!");
+    }
+    //Tijera
+    if (processedInput === "tijera" && randomChoice() === "papel") {
+      console.log("Ganaste!");
+      contHumano++;
+    } else if (processedInput === "tijera" && randomChoice() === "piedra") {
+      console.log("Perdiste!");
+      contPc++;
+    } else if (processedInput === "tijera" && randomChoice() === "tijera") {
+      console.log("Empate!");
+    }
+  }
+  //Evaluar resultados
+  if (contHumano > contPc) {
+    console.log("Ganaste el juego!");
+    console.log("Puntaje final - Humano:", contHumano, "PC:", contPc);
+  } else if (contHumano < contPc) {
+    console.log("Perdiste el juego!");
+    console.log("Puntaje final - Humano:", contHumano, "PC:", contPc);
+  } else {
+    console.log("El juego terminó en empate!");
+    console.log("Puntaje final - Humano:", contHumano, "PC:", contPc);
+  }
 }
+playGame();
