e1e3a973866289b9e6b3309603e6a4f8b255f9d7
Hiuvek <160373690+Hiuvek@users.noreply.github.com>
2026-08-04 20:40:24 -0600

    RandomChoiseFuction

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ function getUserInput() {
   const userInput = prompt("Please enter your input:");
   return userInput;
 }
+// Normalizar texto a minúsculas
 function processInput(userInput) {
-  // Normalizar texto a minúsculas
   const normalizedInput = userInput.toLowerCase();
   return normalizedInput;
 }
-getUserInput();
-let input = processInput(getUserInput());
-console.log("User Input:", input);
+function randomChoice() {
+  const randomInt = Math.floor(Math.random() * (4 - 1)) + 1;
+  return randomInt;
+}
+for (let i = 0; i < 3; i++) {
+  let userInput = getUserInput();
+  let processedInput = processInput(userInput);
+  console.log("User Input:", processedInput);
+  console.log("Random Choice:", randomChoice());
+}
