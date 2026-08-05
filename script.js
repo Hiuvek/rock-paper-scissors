61f338eb7ec194a03f3a3736eeb708aa7f3e004e
Hiuvek <160373690+Hiuvek@users.noreply.github.com>
2026-08-04 20:31:31 -0600

    Normalize text

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getUserInput() {
+  const userInput = prompt("Please enter your input:");
+  return userInput;
+}
+function processInput(userInput) {
+  // Normalizar texto a minúsculas
+  const normalizedInput = userInput.toLowerCase();
+  return normalizedInput;
+}
+getUserInput();
+let input = processInput(getUserInput());
+console.log("User Input:", input);
